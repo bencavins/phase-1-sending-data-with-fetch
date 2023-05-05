72472bea5c257fae252b5c4805b8be881603d395
Ben Cavins <ben.cavins@flatironschool.com>
2023-05-05 14:52:07 -0400

    do lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            'accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        const newId = data.id
+        const p = document.createElement('p')
+        p.textContent = newId
+        document.querySelector('body').appendChild(p)
+    })
+    .catch(error => {
+        const msg = error.message
+        const p = document.createElement('p')
+        p.textContent = msg
+        document.querySelector('body').appendChild(p)
+    })
+}
